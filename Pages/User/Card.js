861cd0beb7a0d4693eebae75d6dd5126262fc0ec
berkeodaci <berkeodaci@sabanciuniv.edu>
2022-04-12 15:04:35 +0300

    cinsityet fix

--- Pages/User/Card.js
+++ Pages/User/Card.js
@@ -520,7 +520,7 @@ export default Card = ({
 														paddingVertical: 5,
 													}}
 												>
-													Cinsityet{"\n"}
+													Cinsiyet{"\n"}
 													<Text style={{ fontFamily: "PoppinsSemiBold", fontSize: 22 }}>
 														{gender}
 													</Text>
