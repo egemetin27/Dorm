8c37431b2111217a70c9b7ed3c0e2a5f6bbe637e
egemetin <egemetin@sabanciuniv.edu>
2022-08-19 12:49:49 +0200

    Fixed today in chats if they are from yesterday

--- utils/date.utils.js
+++ utils/date.utils.js
@@ -19,18 +19,32 @@ export const getWhen = (date) => {
 	var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
 
 	var dayDifference;
-	switch (parseInt(Difference_In_Days)) {
-		case 0:
-			dayDifference = "Bugün";
-			break;
-		case 1:
-			dayDifference = "Dün";
-			break;
-		default:
-			dayDifference = "Daha Önce";
-			break;
-	}
+	// switch (parseInt(Difference_In_Days)) {
+	// 	case 0:
+	// 	    dayDifference = "Bugün";
+	// 		break;
+	// 	case 1:
+	// 		dayDifference = "Dün";
+	// 		break;
+	// 	default:
+	// 		dayDifference = "Daha Önce";
+	// 		break;
+	// }
 
+	if (parseInt(Difference_In_Days) == 0 && newDate.getDay() == now.getDay()) {
+	    dayDifference = "Bugün";
+	}
+	else if (parseInt(Difference_In_Days) == 0) {
+		// if the message is from yesterday but no later than 24h
+	    dayDifference = "Dün";
+	}
+	else if (parseInt(Difference_In_Days) == 1 && now.getDay() - newDate.getDay() == 1) {
+		dayDifference = "Dün";
+	}
+	else {
+		dayDifference = "Daha Önce";
+	}
+	
 	return dayDifference;
 };
 
